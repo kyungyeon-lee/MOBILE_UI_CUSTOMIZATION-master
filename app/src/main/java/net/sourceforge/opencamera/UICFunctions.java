59b70e87cea1d1a1e535ecfc5df9d38e6dfb5671
ruddus716 <rudddus716@gmail.com>
2020-02-17 10:03:55 +0900

    complete mainactivity

---
+++ app/src/main/java/net/sourceforge/opencamera/UICFunctions.java
@@ -0,0 +1,73 @@
+package net.sourceforge.opencamera;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.lang.reflect.Method;
+
+public class UICFunctions extends MainActivity {
+
+/*
+    public Activity activity;
+    public View view;
+    static String s1 = null;
+
+    public UICFunctions() {
+
+    }
+
+    public UICFunctions(Activity _activity, View view) {
+        this.activity = _activity;
+        this.view = view;
+    }
+
+    int i = 0;
+
+    public void addBtn() {
+
+        //the layout on which you are working
+        LinearLayout layout = (LinearLayout) activity.findViewById(R.id.layout1);
+        //action copy
+        Button btnTag = new Button(activity.getApplicationContext());
+        btnTag.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        btnTag.setText("" + i);
+        btnTag.setTag("ANDROID ICON");
+        btnTag.setOnLongClickListener((View.OnLongClickListener) activity);
+
+        final String[] functions = {"clickedTakePhoto", "clickedSwitchCamera", "clickedGallery", "clickedSwitchVideo"};
+        btnTag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // clickedGallery(view);
+         //       makeTrans();
+            }
+        });
+        //design
+
+        //locate
+
+        //set the properties for button
+
+        //btnTag.setId("v");
+
+        //add button to the layout
+        layout.addView(btnTag);
+    }
+
+    public void actionBtn() {
+    }
+
+    public void customizeBtn() {
+    }
+
+    public void locateBtn() {
+    }
+
+*/
+
+
+
+}
